eb44fe7403beb2a61d4f47fbc6b1802da362fd9b
chendaiz1 <144401014+chendaiz1@users.noreply.github.com>
2025-04-13 10:28:55 -0400

    remove attribute mode='RGB'

--- photo_wct.py
+++ photo_wct.py
@@ -80,15 +80,15 @@ class PhotoWCT(nn.Module):
             if len(cont_seg.shape) == 2:
                 t_cont_seg = np.asarray(Image.fromarray(cont_seg).resize((cont_w, cont_h), Image.NEAREST))
             else:
-                # Read into gray scale with mode='RGB'
+                # Remove attribute mode='RGB'
                 # t_cont_seg = np.asarray(Image.fromarray(cont_seg, mode='RGB').resize((cont_w, cont_h), Image.NEAREST))
-                t_cont_seg = np.asarray(Image.fromarray(cont_seg, mode='L').resize((cont_w, cont_h), Image.NEAREST))
+                t_cont_seg = np.asarray(Image.fromarray(cont_seg).resize((cont_w, cont_h), Image.NEAREST))
             if len(styl_seg.shape) == 2:
                 t_styl_seg = np.asarray(Image.fromarray(styl_seg).resize((styl_w, styl_h), Image.NEAREST))
             else:
-                # Read into gray scale with mode='RGB'
+                # Remove attribute mode='RGB'
                 # t_styl_seg = np.asarray(Image.fromarray(styl_seg, mode='RGB').resize((styl_w, styl_h), Image.NEAREST))
-                t_styl_seg = np.asarray(Image.fromarray(styl_seg, mode='L').resize((styl_w, styl_h), Image.NEAREST))
+                t_styl_seg = np.asarray(Image.fromarray(styl_seg).resize((styl_w, styl_h), Image.NEAREST))
 
             for l in self.label_set:
                 if self.label_indicator[l] == 0:
